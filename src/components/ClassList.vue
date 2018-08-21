b4109bdcbafdb60af37a2e201b29bf48595ba86d
Andrew Clemens <apc@press.jhu.edu>
2018-08-21 15:11:23 -0400

    add some data and images

--- src/components/ClassList.vue
+++ src/components/ClassList.vue
@@ -4,12 +4,15 @@
             <tr>
                 <td width="5%">
                     Extremely low frequency
+                    <img src="./../assets/elf.jpg">
                 </td>
                 <td width="5%">
                     Super low frequency
+                    <img src="./../assets/slf.svg">
                 </td>
                 <td width="5%">
                     Ultra low frequency
+                    <img src="./../assets/ulf.png">
                 </td>
                 <td width="5%">
                     Very low frequency
@@ -19,45 +22,55 @@
                 </td>
                 <td width="5%">
                     Medium frequency
+                    <img src="./../assets/mf.png">
                 </td>
                 <td width="5%">
                     High frequency
                 </td>
                 <td width="5%">
                     Very high frequency
+                    <img src="./../assets/vhf.png">
                 </td>
                 <td width="5%">
                     Ultra high frequency
+                    <img src="./../assets/uhf.png">
                 </td>
                 <td width="5%">
                     Super high frequency
+                    <img src="./../assets/shf.png">
                 </td>
                 <td width="5%">
                     Extremely high frequency
                 </td>
                 <td width="5%">
                     Far infrared
+                    <img src="./../assets/fi.png">
                 </td>
                 <td width="5%">
                     Mid infrared
+                    <img src="./../assets/mi.jpg">
                 </td>
                 <td width="5%">
                     Near infrared
+                    <img src="./../assets/ni.png">
                 </td>
                 <td width="5%">
                     Near ultraviolet
                 </td>
                 <td width="5%">
                     Extreme ultraviolet
+                    <img src="./../assets/eu.png">
                 </td>
                 <td width="5%">
                     Soft X-rays
                 </td>
                 <td width="5%">
                     Hard X-rays
+                    <img src="./../assets/hx.png">
                 </td>
                 <td width="5%">
                     Gamma rays
+                    <img src="./../assets/gr.png">
                 </td>
             </tr>
         </table>
@@ -75,5 +88,8 @@ export default {
     position: absolute;
     width: 100%;
     height: 100%;
+}
+table img {
+    width: 100%;
 }
 </style>
--- src/components/Information.vue
+++ src/components/Information.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="information" v-bind:style="{transform: 'translateX('+translate+'%)'}">
-        <div class="band" v-for="band in information"
+        <div class="band" v-for="band in information" v-bind:class="{tick: band.start == band.end}"
             v-bind:style="{
                  left: frequency_to_position_information(band.start, scale)*90 + '%',
                  width: (frequency_to_position_information(band.end, scale)*90 - frequency_to_position_information(band.start, scale)*90) + '%'
@@ -29,6 +29,65 @@ export default {
         }
     },
 }
+
+function frequencyToRGB(frequency){
+    var Wavelength = 299792458 / frequency;
+    var factor;
+    var Red,Green,Blue;
+    const Gamma = 0.8;
+    const IntensityMax = 255;
+
+    if((Wavelength >= 380) && (Wavelength<440)){
+        Red = -(Wavelength - 440) / (440 - 380);
+        Green = 0.0;
+        Blue = 1.0;
+    }else if((Wavelength >= 440) && (Wavelength<490)){
+        Red = 0.0;
+        Green = (Wavelength - 440) / (490 - 440);
+        Blue = 1.0;
+    }else if((Wavelength >= 490) && (Wavelength<510)){
+        Red = 0.0;
+        Green = 1.0;
+        Blue = -(Wavelength - 510) / (510 - 490);
+    }else if((Wavelength >= 510) && (Wavelength<580)){
+        Red = (Wavelength - 510) / (580 - 510);
+        Green = 1.0;
+        Blue = 0.0;
+    }else if((Wavelength >= 580) && (Wavelength<645)){
+        Red = 1.0;
+        Green = -(Wavelength - 645) / (645 - 580);
+        Blue = 0.0;
+    }else if((Wavelength >= 645) && (Wavelength<781)){
+        Red = 1.0;
+        Green = 0.0;
+        Blue = 0.0;
+    }else{
+        Red = 0.0;
+        Green = 0.0;
+        Blue = 0.0;
+    };
+
+    if((Wavelength >= 380) && (Wavelength<420)){
+        factor = 0.3 + 0.7*(Wavelength - 380) / (420 - 380);
+    }else if((Wavelength >= 420) && (Wavelength<701)){
+        factor = 1.0;
+    }else if((Wavelength >= 701) && (Wavelength<781)){
+        factor = 0.3 + 0.7*(780 - Wavelength) / (780 - 700);
+    }else{
+        factor = 0.0;
+    };
+
+
+    var rgb = [0,0,0];
+
+    // Don't want 0^x = 1 for x <> 0
+    rgb[0] = Red==0.0 ? 0 : Math.round(IntensityMax * Math.pow(Red * factor, Gamma));
+    rgb[1] = Green==0.0 ? 0 : Math.round(IntensityMax * Math.pow(Green * factor, Gamma));
+    rgb[2] = Blue==0.0 ? 0 : Math.round(IntensityMax * Math.pow(Blue * factor, Gamma));
+
+    return rgb;
+}
+
 </script>
 
 <style>
@@ -45,5 +104,10 @@ export default {
     top: 0;
     border: 2px solid black;
     box-sizing: border-box;
+}
+.tick {
+    width: auto !important;
+    border: none;
+    border-left: 2px solid black;
 }
 </style>
