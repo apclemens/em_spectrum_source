943234cc880c1aa7c2f131c0733efc5cfba53954
Andrew Clemens <apc@press.jhu.edu>
2018-08-27 11:01:32 -0400

    add spectrum information

--- src/components/BandInfo.vue
+++ src/components/BandInfo.vue
@@ -6,14 +6,17 @@
 <script>
 import UHF_US from './../assets/info/uhf-us.json'
 import Classes from './../assets/info/classes.json'
+import US from './../assets/info/us.json'
+
+// https://www.ntia.doc.gov/files/ntia/publications/2003-allochrt.pdf
 
 export default {
     name: 'BandInfo',
     data() {
         var info = [[], [], [], []];
         var jsons = [
-            UHF_US,
             Classes,
+            US,
         ];
         var band;
         for (var i=0; i<jsons.length; i++) {
